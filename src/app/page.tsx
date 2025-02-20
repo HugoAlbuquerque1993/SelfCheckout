ee075e5551c929ec8c32475b39074cbc3382c0fa
HugoAlbuquerque1993 <hugo349966@hotmail.com>
2025-02-20 11:57:20 -0300

    chore: add eslint plugin and prettier tailwind plugin

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,14 @@
-import { Button } from "@/components/ui/button"
-import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 
 const HomePage = () => {
-	return (
-		<div>
-			<h1>Hello, World!</h1>
-			<Input placeholder="Digite seu nome" />
-			<Button>Confirmar</Button>
-		</div>
-	)
-}
+  return (
+    <div className="flex w-64 flex-col gap-2">
+      <h1 className="text-4xl">Hello, World!</h1>
+      <Input placeholder="Digite seu nome" />
+      <Button>Confirmar</Button>
+    </div>
+  );
+};
 
-export default HomePage
+export default HomePage;
